938d51fa84a6a160b4ce745a643a9e066231d895
Pavan Kumar <pavana004@gmail.com>
2024-11-14 23:27:33 +0530

    commit

--- src/app/student/studentLogin/page.js
+++ src/app/student/studentLogin/page.js
@@ -63,23 +63,24 @@ const StudentLoginPage = () => {
               <div className="flex justify-end">
                 <button className="bg-green-500 text-white font-bold cursor-pointer px-6 py-2 w-24 flex justify-end">
                   {loading ? (
-                    <span className="animate-spin text-white mr-3">
-                      <svg
-                        className="w-4 h-4"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <circle
-                          className="opacity-50"
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="currentColor"
-                          strokeWidth="4"
-                        />
-                      </svg>
-                    </span>
+                    // <span className="animate-spin text-white mr-3">
+                    //   <svg
+                    //     className="w-4 h-4"
+                    //     fill="none"
+                    //     viewBox="0 0 24 24"
+                    //     xmlns="http://www.w3.org/2000/svg"
+                    //   >
+                    //     <circle
+                    //       className="opacity-50"
+                    //       cx="12"
+                    //       cy="12"
+                    //       r="10"
+                    //       stroke="currentColor"
+                    //       strokeWidth="4"
+                    //     />
+                    //   </svg>
+                    // </span>
+                    <h6>loading..</h6>
                   ) : (
                     "Login"
                   )}
